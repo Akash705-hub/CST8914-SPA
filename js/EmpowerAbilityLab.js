842803e8cd64c2244ec2ccac48ab28ad0e35889d
Akash705-hub <pate1706@algonquinlive.com>
2026-04-13 00:17:08 -0400

    Implement 404 redirect logic
    Add redirect functionality for 404 errors.

--- js/EmpowerAbilityLab.js
+++ js/EmpowerAbilityLab.js
@@ -1,3 +1,12 @@
+// Redirects user from 404
+(function () {
+    var redirect = sessionStorage.redirect;
+    delete sessionStorage.redirect;
+    if (redirect && redirect != location.href) {
+        history.replaceState(null, null, redirect);
+    }
+})();
+
 /**
  * Main initialization function that sets up all interactive components
  * for the Single Page Application
